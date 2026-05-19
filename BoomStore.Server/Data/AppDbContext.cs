a8f6f5853f40c4a55c5a8249c314a2ac88bc8106
xamar-sharp <xamacoredevelopment@gmail.com>
2026-05-19 22:02:53 +0300

    first

--- BoomStore.Server/Data/AppDbContext.cs
+++ BoomStore.Server/Data/AppDbContext.cs
@@ -14,24 +14,94 @@ public class AppDbContext : DbContext
     {
         base.OnModelCreating(modelBuilder);
         modelBuilder.Entity<Category>().HasData(
+new Category
+{
+    Id = 1,
+    Name = "Смартфоны"
+},
+            new Category
+            {
+                Id = 2,
+                Name = "Ноутбуки"
+            },
 
-        new Category
-        {
-            Id = 1,
-            Name = "Смартфоны"
-        },
+            new Category
+            {
+                Id = 3,
+                Name = "Наушники"
+            },
 
-        new Category
-        {
-            Id = 2,
-            Name = "Ноутбуки"
-        },
+            new Category
+            {
+                Id = 4,
+                Name = "Планшеты"
+            },
 
-        new Category
-        {
-            Id = 3,
-            Name = "Наушники"
-        }
+            new Category
+            {
+                Id = 5,
+                Name = "Смарт-часы"
+            },
+
+            new Category
+            {
+                Id = 6,
+                Name = "Мониторы"
+            },
+
+            new Category
+            {
+                Id = 7,
+                Name = "Игровые консоли"
+            },
+
+            new Category
+            {
+                Id = 8,
+                Name = "Телевизоры"
+            },
+
+            new Category
+            {
+                Id = 9,
+                Name = "Клавиатуры"
+            },
+
+            new Category
+            {
+                Id = 10,
+                Name = "Компьютерные мыши"
+            },
+
+            new Category
+            {
+                Id = 11,
+                Name = "Видеокарты"
+            },
+
+            new Category
+            {
+                Id = 12,
+                Name = "Процессоры"
+            },
+
+            new Category
+            {
+                Id = 13,
+                Name = "Колонки"
+            },
+
+            new Category
+            {
+                Id = 14,
+                Name = "Веб-камеры"
+            },
+
+            new Category
+            {
+                Id = 15,
+                Name = "Микрофоны"
+            }
     );
     }
     public DbSet<User> Users => Set<User>();
